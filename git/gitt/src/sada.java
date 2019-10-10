891083d35912102c146c4760e9821d267c4cb5bf
zhangwei <287277737@qq.com>
2019-10-10 20:31:03 +0800

    添加test工程到git

--- git/gitt/src/sada.java
+++
@@ -1,7 +0,0 @@
-public class sada {
-    public static void main(String[] args) {
-        System.out.println("1111");
-        System.out.println("1111");
-        System.out.println("1111d");
-    }
-}
