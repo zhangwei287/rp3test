27c668e4fcd7119c0be4540a081fc41f84c31f86
zhangwei <287277737@qq.com>
2019-12-12 01:27:02 +0800

    修改

--- gitt/src/C.java
+++ gitt/src/C.java
@@ -1,9 +1,12 @@
 public class C {
     public static void main(String[] args) {
+
+        //修改
         System.out.println("123");
     }
     public  void test(){
         int i =0;
         i+=10;
+        System.out.println(i);
     }
 }
